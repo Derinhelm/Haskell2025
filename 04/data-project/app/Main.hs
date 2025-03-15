c9d48b6ea505d4bc1484165b6eef794607910c81
Shamaeva Elena <derinhelm@yandex.ru>
2025-03-15 15:21:29 +0300

    Adding homework

--- 04/data-project/app/Main.hs
+++ 04/data-project/app/Main.hs
@@ -4,4 +4,6 @@ import MyData
 import Lib
 
 main :: IO ()
-main = putStrLn $ show $ eval (Not (And (Var "x") (Value True))) [("y", True)]
+main = do
+    putStrLn $ show $ eval (Not (And (Var "x") (Value True))) [("x", True)]
+    putStrLn $ show $ eval (Not (And (Var "x") (Value True))) [("y", True)]
--- 04/data-project/src/Lib.hs
+++ 04/data-project/src/Lib.hs
@@ -4,11 +4,19 @@ module Lib
 
 import MyData
 
-eval :: Expr -> [(String, Bool)] -> Bool
-eval (Value a) _ = a
-eval (Not a) vars = not $ eval a vars
-eval (Or a b) vars = (eval a vars) || (eval b vars)
-eval (And a b) vars = (eval a vars) && (eval b vars)
+eval :: Expr -> [(String, Bool)] -> Either String Bool
+eval (Value a) _ = Right a
+eval (Not a) vars = case eval a vars of
+                        Left e -> Left e
+                        Right x -> Right $ not x
+eval (Or a b) vars = binFun (||) (eval a vars) (eval b vars)
+eval (And a b) vars = binFun (&&) (eval a vars) (eval b vars)
 eval (Var s) vars = case lookup s vars of 
-                            Just w -> w
-                            Nothing -> True
+                        Just w -> Right w
+                        Nothing -> Left $ "No var " ++ s
+
+binFun :: (Bool -> Bool -> Bool) -> Either String Bool -> Either String Bool -> Either String Bool
+binFun _ (Left e1) (Left e2) = Left $ e1 ++ ". " ++ e2
+binFun _ (Left e1) _ = Left $ e1
+binFun _ _ (Left e2) = Left $ e2
+binFun f (Right x1) (Right x2) = Right $ f x1 x2
